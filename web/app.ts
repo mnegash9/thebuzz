51dab7804c5fabc45cd1edc6d970690304c793f1
mnegash9 <111900361+mnegash9@users.noreply.github.com>
2025-01-10 04:05:25 -0600

    small typo in url

--- web/app.ts
+++ web/app.ts
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const baseUrl = 'http://matyas.linuxhomeserver.org';
+    const baseUrl = 'http://matyas.homelinuxserver.org';
     const feedContainer = document.querySelector('.feedContainer');
 
     const userId = getQueryParam('user_id');
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
             if (response.ok) {
-                window.location.href = 'http://matyas.linuxhomeserver.org/signin.html';
+                window.location.href = 'http://matyas.homelinuxserver.org/signin.html';
             } else {
                 console.error('Logout failed');
             }
