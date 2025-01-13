4d45f108e21af0ad3ed98de1daf548ffc4089ef4
mnegash9 <111900361+mnegash9@users.noreply.github.com>
2025-01-12 20:39:18 -0600

    updating test scirpt

--- deploy/deploy_dokku.sh
+++ deploy/deploy_dokku.sh
@@ -2,18 +2,19 @@
 set -x
 git branch -D deploy-dokku
 git push -d origin deploy-dokku
-read -p "Press enter to continue"
+read -p "Go to backend branch"
 git switch backend
-read -p "Press enter to continue"
+read -p "Create new branch"
 cd ..
 git switch --create deploy-dokku
-read -p "Press enter to continue"
+read -p "Remove files"
 rm -rf README.md admin-cli/ docs/ mobile/ web/
 read -p "Press enter to continue"
 mv ./backend/* ./
 rm -rf backend/
 git status
-read -p "Press enter to continue"
+read -p "Press enter to commit and push"
 git add .
 git commit -m "backend as root of dokku"
+git push
 git push dokku deploy-dokku:main -f
